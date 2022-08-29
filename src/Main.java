e428e4e3d86f28e4fccc4b5d55434015b1249d76
denizburcu <denizburcua@gmail.com>
2022-08-29 23:36:01 +0300

    Vehicle, Car, Outlander created and tested main method.

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,15 @@ public class Main {
         dog.eat();
         dog.walk();
         dog.move(20);
+
+
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+
+
     }
 
 }
